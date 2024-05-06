a74c98cbcaa569b846a1ca2440f5b8f612a3743f
Marek Hanuš <marekdiamo@gmail.com>
2024-05-06 16:40:32 +0200

    Revert docs, PEP
    ```
    PEP 8: E128 continuation line under-indented for visual indent
    ```

--- Lib/ensurepip/__init__.py
+++ Lib/ensurepip/__init__.py
@@ -122,8 +122,8 @@ def bootstrap(*, root=None, upgrade=False, user=False,
 
 
 def _bootstrap(*, root=None, upgrade=False, user=False,
-              altinstall=False, default_pip=False,
-              verbosity=0):
+               altinstall=False, default_pip=False,
+               verbosity=0):
     """
     Bootstrap pip into the current Python installation (or the given root
     directory). Returns pip command status code.
@@ -137,15 +137,17 @@ def _bootstrap(*, root=None, upgrade=False, user=False,
 
     _disable_pip_configuration_settings()
 
-    # By default, the scripts pipX, pipX.Y will be installed
-    # (X.Y == running Python version).
+    # By default, installing pip installs all of the
+    # following scripts (X.Y == running Python version):
+    #
+    #   pip, pipX, pipX.Y
     #
     # pip 1.5+ allows ensurepip to request that some of those be left out
     if altinstall:
-        # omit pipX
+        # omit pip, pipX
         os.environ["ENSUREPIP_OPTIONS"] = "altinstall"
     elif not default_pip:
-        # include pip
+        # omit pip
         os.environ["ENSUREPIP_OPTIONS"] = "install"
 
     with tempfile.TemporaryDirectory() as tmpdir:
